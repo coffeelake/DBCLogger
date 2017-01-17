20d40c8794f187170445c48e3bc85b6dd9774132
Daniel Berg <daniel.berg@l58.se>
2017-01-17 18:37:37 +0100

    Add start file

---
+++ src/DBCLogger/DBCLogger.ino
@@ -0,0 +1,18 @@
+/*  Arduino DBCLogger
+    Program made by Daniel Berg
+*/
+
+//==== Defining Variables
+char currentPage, selectedUnit;
+void setup() {
+  // Initial setup
+  // Defining Pin Modes
+}
+
+void loop() {
+  // homescreen
+  if (currentPage == '0') {
+
+
+  }
+}
